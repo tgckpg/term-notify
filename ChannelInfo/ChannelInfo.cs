1e1d69449f7af4575cfcdb13141eca5ce5d794c1
斟酌 鵬兄 <tgckpg@gmail.com>
2016-02-13 03:42:48 +0800

    Settings for github

---
+++ ChannelInfo/ChannelInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Channel
+{
+    class Info
+    {
+        public const string NOTIS_PROTO = "https://<YOUR_SERVICE_URI>/";
+        public const string SERVICE_AUTH = "pass=<YOUR_SERVICE_AUTH>&";
+
+    }
+}
